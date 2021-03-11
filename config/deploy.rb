97311cc6bd508b263c2eb639a29c2556a9aff0d5
Edmilson Moreira <edmilson.mlf@gmail.com>
2021-03-11 16:43:01 -0300

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
-require 'mina/rails'
-require 'mina/git'
+#require 'mina/rails'
+#require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-require 'mina/rvm'    # for rvm support. (https://rvm.io)
+#require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -9,18 +9,38 @@ require 'mina/rvm'    # for rvm support. (https://rvm.io)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :application_name, 'Panorama Ceará'
-set :domain, '172.26.41.106'
-set :user, 'sedet'
-set :deploy_to, '/var/www/panorama.com'
-set :repository, 'git@github.com:Thiago-Gomez/panorama.git'
-set :branch, 'master'
+server '172.26.41.106', port: 22, roles: [:web, :app, :db], primary: true
+
+set :application_name,  'Panorama Ceará'
+set :domain,            '172.26.41.106'
+set :application,       'panorama'
+set :user,               'sedet'
+#set :deploy_to,          '/var/www/panorama.com'
+set :repo_url,           'git@github.com:Thiago-Gomez/panorama.git'
+#set :repository,         'git@github.com:Thiago-Gomez/panorama.git'
+#set :branch,             'master'
 
 
 # Optional settings:
 #   set :user, 'foobar'          # Username in the server to SSH to.
 #   set :port, '30000'           # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
@@ -32,59 +52,103 @@ set :branch, 'master'
 # `mina deploy` or `mina rake`.
 
 #set :rvm_use_path, '/usr/local/rvm/scripts/rvm'
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
 
-set :rvm_path, '/usr/local/rvm/scripts/rvm'
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+#set :rvm_path, '/usr/local/rvm/scripts/rvm'S
 
 
-task :remote_environment do
+#task :remote_environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .ruby-version or .rbenv-version to your repository.
  # invoke :'rbenv:load'
 
- invoke :'rvm:use', 'ruby-3.0.0@panorama'
+ #invoke :'rvm:use', 'ruby-3.0.0@panorama'
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
 
 
 
-end
+#end
 
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
-task :setup do
+#task :setup do
   # command %{rbenv install 2.3.0 --skip-existing}
-end
+#end
 
-desc "Deploys the current version to the server."
-task :deploy do
+#desc "Deploys the current version to the server."
+#task :deploy do
   # uncomment this line to make sure you pushed your local branch to the remote origin
   # invoke :'git:ensure_pushed'
-  deploy do
+  #deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
 
     # 1 - pull application server
-    invoke :'git:clone'
+    #invoke :'git:clone'
 
     # 2 - criar links compartilhados
-    invoke :'deploy:link_shared_paths'
+    #invoke :'deploy:link_shared_paths'
 
     # 3 - bundle install
-    invoke :'bundle:install'
+    #invoke :'bundle:install'
 
 
     # 4 - rake db migrate
-    invoke :'rails:db_migrate'
+    #invoke :'rails:db_migrate'
 
     # 5 - precompile assets
-    invoke :'rails:assets_precompile'
+    #invoke :'rails:assets_precompile'
 
  
     # 6 - deploy clean up old releases
-    invoke :'deploy:cleanup'
+    #invoke :'deploy:cleanup'
 
-    on :launch do
+    #on :launch do
       
       # invoke :'puma:phased_restart'
 
@@ -96,14 +160,14 @@ task :deploy do
       # #   command 'touch tmp/restart.txt'
       # # end
 
-    end
+    #end
 
 
-  end
+  #end
 
   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
   # run(:local){ say 'done' }
-end
+#end
 
 # For help in making your deploy script, see the Mina documentation:
 #
